4eff7132ca1346ee6f5240eb49756a7981ef48d9
PURPUR124 <eustolioea@gmail.com>
2022-12-08 10:04:01 -0700

    Added conclusion

--- dayChecker.py
+++ dayChecker.py
@@ -9,7 +9,6 @@ elif HowIsDoingToday == "Good":
     print("That's great! I hope it stays that way " + Name + "!")
 else: 
     print("I'm not sure what you meant by that, but ok?")
-## ughhhhh it thinks everything is pizza. TBFL
 FavFood = input("What's your favorite food? I think mine might be pizza! ")
 if FavFood == "Pizza":
     print("Oh wow we have the same favorite food, Pizza! :0")
@@ -19,12 +18,11 @@ elif FavFood == "Sushi":
     print("Mmmmmm sushii. Sushi's reeeeaally good too")
 else:
     print("Hmm, I haven't tried that! It sounds like it could be good though!")
-
-##UserAge = input("How old are you? I'm preetty young, I mean, I was just made! ")
-##if UserAge == "1" or "2" or "3" or "4" or "5":
-   ##print("Oh! You're REALLY young! You spell really well! Keep it up!")
-##elif UserAge == "6" or "7" or "8" or "9" or "10":
-    ##print("You're getting older! Congrats!")
-##else:
-    ##print("You're oldddd")
+print("I've been doing some random stuff lately, namely gaming.")
+WhatBeenDoingLately = input("How about you? ")
+if WhatBeenDoingLately == "Nothing really":
+    print("Oh that's too bad, maybe you can do something fun sometime soon?")
+else:
+    print("Oooh!")
+print("Well, I had fun talking to you! I hope you have a good rest of your day :)")
 
