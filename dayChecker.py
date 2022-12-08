c67c8eb0878e071f323fc6014398942232038adf
PURPUR124 <eustolioea@gmail.com>
2022-12-07 20:50:19 -0700

    REEEEEE

---
+++ dayChecker.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python3
+
+Name = input("What's your name? ")
+print("Nice to meet you, " + Name + "!")
+HowIsDoingToday = input("How are you doing today " + Name + "?? Good or Bad? ")
+if HowIsDoingToday == "Bad":
+    print("Oh I'm so sorry to hear that, " + Name + "! I hope it gets better!")
+elif HowIsDoingToday == "Good":
+    print("That's great! I hope it stays that way " + Name + "!")
+
+
+
