63b6cf8311fa213fdd9bd089ddc73cab16337943
PURPUR124 <eustolioea@gmail.com>
2022-12-07 21:15:20 -0700

    Getting rid of stupid friggin comments

--- dayChecker.py
+++ dayChecker.py
@@ -1,12 +1,7 @@
 #!/usr/local/bin/python3
 
-## get user input
 Name = input("What's your name? ")
-
-## print some things
 print("Nice to meet you, " + Name + "!")
-
-## get _more_ input
 HowIsDoingToday = input("How are you doing today " + Name + "?? Good or Bad? ")
 if HowIsDoingToday == "Bad":
     print("Oh I'm so sorry to hear that, " + Name + "! I hope it gets better!")
