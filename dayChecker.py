10b05bfbce82862f03f6f7316740a820a5e17acd
PURPUR124 <eustolioea@gmail.com>
2022-12-07 21:54:42 -0700

    Adding age and favorite food components, still slightly buggy tho

--- dayChecker.py
+++ dayChecker.py
@@ -9,6 +9,21 @@ elif HowIsDoingToday == "Good":
     print("That's great! I hope it stays that way " + Name + "!")
 else: 
     print("I'm not sure what you meant by that, but ok?")
+## ughhhhh it thinks everything is pizza. TBFL
+FavFood = input("What's your favorite food? I think mine might be pizza! ")
+if FavFood == "Pizza" or "pizza":
+    print("Oh wow we have the same favorite food, Pizza! :0")
+    print("Thats cool dontcha think??")
+elif FavFood == "Sandwiches" or "sandwiches":
+    print("I like sandwiches too! Unless they get soggy, ew.")
+elif FavFood == "Sushi" or "sushi":
+    print("Mmmmmm sushii. Sushi's reeeeaally good too")
+else:
+    print("Hmm, I haven't tried that! It sounds like it could be good though!")
 
-
+UserAge = input("How old are you? I'm preetty young, I mean, I was just made! ")
+if UserAge == "1" or "2" or "3" or "4" or "5":
+    print("Oh! You're REALLY young! You spell really well! Keep it up!")
+elif UserAge == "6" or "7" or "8" or "9" or "10":
+    print("You're getting older! Congrats!")
 
