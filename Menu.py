340673d3e185cc1e060ece19dc384f06219f8332
PURPUR124 <eustolioea@gmail.com>
2022-12-13 15:57:20 -0700

    IT WORKSSS

--- Menu.py
+++ Menu.py
@@ -1,5 +1,5 @@
 #!/usr/local/bin/python3
-
+import os
 
 
 food = ["Apple.py","If Else Eclair"]
@@ -20,24 +20,36 @@ while True:
 
     choice = input("Enter your menu selection: ")
     if (choice == "1"):
+        # Clearing the screen using the imported 'os' library
+        os.system('clear')
         print ("Thank you for visiting the Wonderful Python Cafe, please enjoy your Else If Eclair!")
+        os.system('sleep 2')
         print("Here's our menu again incase you forgot")
     elif (choice == "2"):
+        os.system('clear')
         print ("Thank you for visiting the Wonderful Python Cafe, please enjoy your Elif Latte!")
+        os.system('sleep 2')
         print("Here's our menu again incase you forgot")
     elif (choice == "3"):
+        os.system('clear')
         print ("Thank you for visiting the Wonderful Python Cafe, please enjoy your While Loop Tea!")
+        os.system('sleep 2')
         print("Here's our menu again incase you forgot")
     elif (choice == "4"):
+        os.system('clear')
         print ("Thank you for visiting the Wonderful Python Cafe, please enjoy your Apple.py!")
+        os.system('sleep 2')
         print("Here's our menu again incase you forgot")
     elif (choice == "5"):
+        os.system('clear')
         print("You left, and are now on the sidewalk of a small bustling town.")
         break
     elif (choice == "6"):
+        os.system('clear')
         WorkForCafe = input("Oh you want to work for the cafe? (Y)es or (N)o: ")
         if WorkForCafe == ('Y') or ('y'):
             while WorkForCafe == "Y":
+                os.system('clear')
                 print("Thanks for coming in today!")
                 print("You have a couple of tasks you can do today:")
                 print("1.) Wash Dishes")
@@ -49,30 +61,44 @@ while True:
                 choice2 = input("What would you like to do? ")
 
                 if choice2 == "1":
+                    os.system('clear')
                     print("You washed dishes, and they SPARKLE")
                 elif choice2 == "2":
+                    os.system('clear')
                     print("You prepped food, and it tastes AMAZING according to our customers")
                 elif choice2 == "3":
+                    os.system('clear')
                     print("You prepped drinks, and they taste INCREDIBLE according to our customers")
                 elif choice2 == "4":
+                    os.system('clear')
                     print("You swept, and now the floor isn't dusty!")
                 elif choice2 == "5":
+                    os.system('clear')
                     print("You mopped, and now the floor isn't sticky!")
                 elif choice2 == "6":
+                    os.system('clear')
                     print("You stopped working for us!")
                     break
                 else:
+                    os.system('clear')
                     print("That's not on our task list, sorry!")
 while True:
+    os.system('clear')
     print("You can do two things right now")
     print("1.) Walk down the street")
     print("2.) Check what buisnesses there are along the street")
-    WhatToDo = input("What would you like to do?")
+    WhatToDo = input("What would you like to do? ")
     if WhatToDo == "2":
+        os.system('clear')
         print("You see 4 buisnesses, and 3 homes. The buisnesses are named A, B, and C")
-        EnterBuisness = input("What buisness would you like to enter? A, B, or C?")
+        EnterBuisness = input("What buisness would you like to enter? A, B, or C? ")
         if EnterBuisness == "A":
             print("blah")
+        if EnterBuisness == "B":
+            print("blah blah")
+        if EnterBuisness == "C":
+            print("blah blah blah")
+
 print("")
 print("")
 print("")
